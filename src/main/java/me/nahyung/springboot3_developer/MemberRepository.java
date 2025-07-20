4bdbe7e7ea895d7f8260803aac4f8bffc6855fd2
Lee-nahyung <skgud0125@naver.com>
2025-07-20 22:23:44 +0900

    블로그 프로젝트 생성을 위한, 기존 테스트 코드들 삭제

--- src/main/java/me/nahyung/springboot3_developer/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package me.nahyung.springboot3_developer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
