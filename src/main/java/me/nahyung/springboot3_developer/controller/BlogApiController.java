e110039245864cd4cc1f2eb260ba02af3fef1257
Lee-nahyung <skgud0125@naver.com>
2025-07-23 22:03:43 +0900

    BlogApiController 작성

---
+++ src/main/java/me/nahyung/springboot3_developer/controller/BlogApiController.java
@@ -0,0 +1,28 @@
+package me.nahyung.springboot3_developer.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.nahyung.springboot3_developer.AddArticleRequest;
+import me.nahyung.springboot3_developer.domain.Article;
+import me.nahyung.springboot3_developer.service.BlogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController //HTTP Response Body에 HTTP 응답으로 객체 데이터를 JSON 형식으로 반환
+public class BlogApiController {
+
+    private final BlogService blogService;
+
+    //HTTP메서드가 POST일 때 전달받은 URL과 동일하면 메서드로 매핑
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticles(@RequestBody AddArticleRequest request){
+        Article saveArticle = blogService.save(request);
+
+        //요청한 자원이 성공적으로 생성되었으며, 저장된 블로그 글 정보를 응답 객체에 담아 전송
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(saveArticle);
+    }
+}
