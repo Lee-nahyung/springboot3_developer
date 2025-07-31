c2c459eae9720a09aa6b4b186b3efd376970a683
Lee-nahyung <skgud0125@naver.com>
2025-07-31 21:48:00 +0900

    블로그 글 전체 조회 API

--- src/main/java/me/nahyung/springboot3_developer/controller/BlogApiController.java
+++ src/main/java/me/nahyung/springboot3_developer/controller/BlogApiController.java
@@ -3,12 +3,13 @@ package me.nahyung.springboot3_developer.controller;
 import lombok.RequiredArgsConstructor;
 import me.nahyung.springboot3_developer.dto.AddArticleRequest;
 import me.nahyung.springboot3_developer.domain.Article;
+import me.nahyung.springboot3_developer.dto.ArticleResponse;
 import me.nahyung.springboot3_developer.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController //HTTP Response Body에 HTTP 응답으로 객체 데이터를 JSON 형식으로 반환
@@ -24,5 +25,16 @@ public class BlogApiController {
         //요청한 자원이 성공적으로 생성되었으며, 저장된 블로그 글 정보를 응답 객체에 담아 전송
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(saveArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
